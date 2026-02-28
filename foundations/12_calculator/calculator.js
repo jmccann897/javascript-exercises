f12734e550b13bd1e62ac396ddfea7e3a6ec4d7b
Josh <55717114+jmccann897@users.noreply.github.com>
2026-02-28 21:55:44 +0000

    ex 11 to 13

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  let toReturn = 0;
+	nums.forEach(element => {
+      toReturn += element;
+  });
+  return toReturn;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let toReturn = 1;
+	nums.forEach(element => {
+      toReturn *= element;
+  });
+  return toReturn;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  return base**power;
 };
 
-const factorial = function() {
+const factorial = function(num) {
 	
+  // 0
+  if (num===0) {
+    console.log("inside the num===0 if block");
+    return 1;
+  }
+  // >=1
+  let toReturn=1;
+  for (let i = num; i > 0; i--){
+    console.log("inside the for block");
+    toReturn*=i;
+    console.log("toReturn", toReturn);
+  }
+  return toReturn;
 };
 
 // Do not edit below this line
--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (s) {
+    let noPunct = s.replace(/[^\w\s\']|_/g, "");
+    let lower = noPunct.toLowerCase();
+    let trimmedString = lower.trim();
 
+    let noSpaces = trimmedString.replace(/\s/g, "");
+    
+    console.log("no spaces", noSpaces);
+
+    let reversedString = noSpaces.split("").reverse().join("");
+
+    console.log("reversed", reversedString);
+
+    let isPalindrome = false; // default to false
+
+    if(noSpaces == reversedString){
+        isPalindrome = true;
+    }
+
+    return isPalindrome;
 };
 
 // Do not edit below this line
