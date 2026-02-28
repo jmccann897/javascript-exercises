fa85469c0a9331e77ad4faef8cb054d5183e2f65
Josh <55717114+jmccann897@users.noreply.github.com>
2026-02-28 11:59:21 +0000

    ex 9 & 10

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let leap = false; // default to false
 
+    // div by 4&&not by 100 OR div by 100&&400 == leap
+
+    // div by 100 != leap 
+
+
+    if (year%4 == 0 && year%100 != 0 || year%100 ==0 && year%400 == 0){
+        leap = true;
+    }
+
+    return leap;
 };
 
 // Do not edit below this line
