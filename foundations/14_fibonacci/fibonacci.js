cb8d66f7b0ed460d29e2b7e6f5e5fd3f8b04a02e
Josh <55717114+jmccann897@users.noreply.github.com>
2026-03-11 20:50:19 +0000

    ex 13

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS"
+    }
 
+    // create array of length num
+    // add 0 to position 0
+    // add 1 to position 1
+    const fibArr = [0, 1];
+
+    // calculate remaining indices in loop based on previous two values
+    // calculate fibonacci sequence upto num
+    for (let i = 2 ; i <= num; i++){
+        fibArr[i] = fibArr[i-1]+fibArr[i-2];
+    }
+
+    return fibArr[num];
 };
 
 // Do not edit below this line
