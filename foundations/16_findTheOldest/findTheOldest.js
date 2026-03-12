07e410041a7b3a97f81884b2dce1a35327a1cadd
Josh <55717114+jmccann897@users.noreply.github.com>
2026-03-12 13:29:00 +0000

    foundations ex 15&16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+const getAge = function(birth, death){
+    if(!death){
+        death = new Date().getFullYear(); 
+    }
+    return death - birth;
+};
 
+const findTheOldest = function(people) {
+
+    const ages = [];
+    people.forEach(person => {
+        if (!person.yearOfBirth)
+        {
+            console.log("in if check- birth:", person.yearOfBirth);
+            ages.push(0);
+
+        }
+        else 
+        {
+            ages.push(getAge(person.yearOfBirth, person.yearOfDeath));
+        }
+    });
+
+    const oldestInd = ages.indexOf(Math.max(...ages));
+    const oldestPerson = people[oldestInd];
+    return oldestPerson;
 };
 
 // Do not edit below this line
